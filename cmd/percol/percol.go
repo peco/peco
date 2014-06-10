f726c5c4fe7ea53df1ab186a18fe26d20baac10e
Daisuke Maki <lestrrat+github@gmail.com>
2014-06-10 11:53:49 +0900

    Update help message to reflect what we actually support now :)

--- cmd/percol/percol.go
+++ cmd/percol/percol.go
@@ -15,39 +15,7 @@ Usage: percol [options] [FILE]
 
 Options:
   -h, --help            show this help message and exit
-  --tty=TTY             path to the TTY (usually, the value of $TTY)
-  --rcfile=RCFILE       path to the settings file
-  --output-encoding=OUTPUT_ENCODING
-                        encoding for output
-  --input-encoding=INPUT_ENCODING
-                        encoding for input and output (default 'utf8')
   --query=QUERY         pre-input query
-  --eager               suppress lazy matching (slower, but display correct
-                        candidates count)
-  --eval=STRING_TO_EVAL
-                        eval given string after loading the rc file
-  --prompt=PROMPT       specify prompt (percol.view.PROMPT)
-  --right-prompt=RIGHT_PROMPT
-                        specify right prompt (percol.view.RPROMPT)
-  --match-method=MATCH_METHOD
-                        specify matching method for query. ` + "`string`" + ` (default)
-                        and ` + "`regex`" + ` are currently supported
-  --caret-position=CARET
-                        position of the caret (default length of the ` + "`query`" + `)
-  --initial-index=INDEX
-                        position of the initial index of the selection
-                        (numeric, "first" or "last")
-  --case-sensitive      whether distinguish the case of query or not
-  --reverse             whether reverse the order of candidates or not
-  --auto-fail           auto fail if no candidates
-  --auto-match          auto matching if only one candidate
-  --prompt-top          display prompt top of the screen (default)
-  --prompt-bottom       display prompt bottom of the screen
-  --result-top-down     display results top down (default)
-  --result-bottom-up    display results bottom up instead of top down
-  --quote               whether quote the output line
-  --peep                exit immediately with doing nothing to cache module
-                        files and speed up start-up time
 `
 	os.Stderr.Write([]byte(v))
 }
