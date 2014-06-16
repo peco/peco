d981e179079b09261397ece8a1008587fb4825f5
Daisuke Maki <lestrrat+github@gmail.com>
2014-06-16 09:15:08 +0900

    Workaround termbox
    termbox's PollEvent() blocks, and on particular instances it blocks
    even after termbox.Close() is called. In order to workaround this,
    we isolate termbox.PollEvent() in a goroutine so we can just wait
    for something to come in via a channel.
    
    In this scenario, once our main functions exit, termbox's polling
    is cleaned up by go itself, and we don't get stuck in that loop.

--- input.go
+++ input.go
@@ -9,12 +9,25 @@ type Input struct {
 func (i *Input) Loop() {
 	defer i.ReleaseWaitGroup()
 
+	// XXX termbox.PollEvent() can get stuck on unexpected signal
+	// handling cases. We still would like to wait until the user
+	// (termbox) has some event for us to process, but we don't
+	// want to allow termbox to control/block our input loop.
+	//
+	// Solution: put termbox polling in a separate goroutine,
+	// and we just watch for a channel. The loop can now
+	// safely be implemented in terms of select {} which is
+	// safe from being stuck.
+	evCh := make(chan termbox.Event)
+	go func() {
+		evCh <- termbox.PollEvent()
+	}()
+
 	for {
 		select {
 		case <-i.LoopCh(): // can only fall here if we closed c.loopCh
 			return
-		default:
-			ev := termbox.PollEvent()
+		case ev := <-evCh:
 			switch ev.Type {
 			case termbox.EventError:
 				//update = false
