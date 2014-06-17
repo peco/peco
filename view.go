9c38b078532c293aee895ac0ce4d16033769d551
Daisuke Maki <lestrrat+github@gmail.com>
2014-06-17 21:35:39 +0900

    some linting... I mean, juuuust a little bit

--- view.go
+++ view.go
@@ -8,20 +8,27 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+// View handles the drawing/updating the screen
 type View struct {
 	*Ctx
 }
 
+// PagingRequest can be sent to move the selection cursor
 type PagingRequest int
 
 const (
+	// ToNextLine moves the selection to the next line
 	ToNextLine PagingRequest = iota
+	// ToNextPage moves the selection to the next page
 	ToNextPage
+	// ToPrevLine moves the selection to the previous line
 	ToPrevLine
+	// ToPrevPage moves the selection to the previous page
 	ToPrevPage
 )
 
-func (u *View) Loop() {
+// Loop receives requests to update the screen
+func (v *View) Loop() {
 	defer u.ReleaseWaitGroup()
 	for {
 		select {
@@ -100,7 +107,7 @@ func (v *View) movePage(p PagingRequest) {
 	v.drawScreen(nil)
 }
 
-func (u *View) drawScreen(targets []Match) {
+func (v *View) drawScreen(targets []Match) {
 	u.mutex.Lock()
 	defer u.mutex.Unlock()
 
