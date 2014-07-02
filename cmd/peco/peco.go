e7b4659f4b479f26e01ef2fc35c79f8ba1fab24b
HIROSE Masaaki <hirose31@gmail.com>
2014-07-03 00:18:25 +0900

    Change number of --initial-index from one-based index to zero-based (same as percol)

--- cmd/peco/peco.go
+++ cmd/peco/peco.go
@@ -24,7 +24,7 @@ Options:
   --no-ignore-case      start in case-sensitive mode
   -b, --buffer-size     number of lines to keep in search buffer
   --null                expect NUL (\0) as separator for target/output (EXPERIMENTAL)
-  --initial-index       position of the initial index of the selection
+  --initial-index       position of the initial index of the selection. 0 is first item
 `
 	os.Stderr.Write([]byte(v))
 }
@@ -38,7 +38,7 @@ type cmdOptions struct {
 	OptVersion       bool   `long:"version" description:"print the version and exit"`
 	OptBufferSize    int    `long:"buffer-size" short:"b" description:"number of lines to keep in search buffer"`
 	OptEnableNullSep bool   `long:"null" description:"expect NUL (\\0) as separator for target/output"`
-	OptInitialIndex  int    `long:"initial-index" description:"position of the initial index of the selection"`
+	OptInitialIndex  int    `long:"initial-index" description:"position of the initial index of the selection. 0 is first item"`
 }
 
 // BufferSize returns the specified buffer size. Fulfills peco.CtxOptions
@@ -52,8 +52,8 @@ func (o cmdOptions) EnableNullSep() bool {
 }
 
 func (o cmdOptions) InitialIndex() int {
-	if o.OptInitialIndex > 0 {
-		return o.OptInitialIndex
+	if o.OptInitialIndex >= 0 {
+		return o.OptInitialIndex + 1
 	} else {
 		return 1
 	}
