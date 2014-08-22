b7605254a77d6aa2d7ffe1f49f32bd86f1abee1c
Daisuke Maki <lestrrat+github@gmail.com>
2014-08-22 14:27:23 +0900

    Apply conversion

--- cmd/peco/peco.go
+++ cmd/peco/peco.go
@@ -100,7 +100,7 @@ func main() {
 	}
 
 	if opts.OptLayout != "" {
-		if ! peco.IsValidLayoutType(opts.OptLayout) {
+		if ! peco.IsValidLayoutType(peco.LayoutType(opts.OptLayout)) {
 			fmt.Fprintf(os.Stderr, "Unknown layout: '%s'\n", opts.OptLayout)
 			st = 1
 			return
