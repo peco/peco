98b19505e5571ac7a15ef4338f5d915e5d20263e
Daisuke Maki <lestrrat+github@gmail.com>
2014-09-08 10:26:53 +0900

    version++; this is v0.2.9

--- cmd/peco/peco.go
+++ cmd/peco/peco.go
@@ -11,7 +11,7 @@ import (
 	"github.com/peco/peco"
 )
 
-var version = "v0.2.8"
+var version = "v0.2.9"
 
 type cmdOptions struct {
 	OptHelp           bool   `short:"h" long:"help" description:"show this help message and exit"`
