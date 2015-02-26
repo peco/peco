504b234a6e3e16dc3b30deca49a1ab4c3c2789d9
Daisuke Maki <lestrrat+github@gmail.com>
2015-02-26 19:58:30 +0900

    Make the comparisons for mergeMatches stricter

--- matchers.go
+++ matchers.go
@@ -373,8 +373,21 @@ func matchOverlaps(a []int, b []int) bool {
 
 func mergeMatches(a []int, b []int) []int {
 	ret := make([]int, 2)
-	ret[0] = a[0]
-	ret[1] = b[1]
+
+	// Note: In practice this should never happen
+	// because we're sorting by N[0] before calling
+	// this routine, but for completeness' sake...
+	if a[0] < b[0] {
+		ret[0] = a[0]
+	} else {
+		ret[0] = b[0]
+	}
+
+	if a[1] < b[1] {
+		ret[1] = b[1]
+	} else {
+		ret[1] = a[1]
+	}
 	return ret
 }
 
