c00f0d29753fd26e7840c7c3834d92ddfe6f8d67
Eric F <eric.fouillet@gmail.com>
2016-12-08 23:35:09 +1100

    Implement #242: Add case sensitivity test cases

--- filter_test.go
+++ filter_test.go
@@ -12,6 +12,10 @@ func TestFuzzyFilter(t *testing.T) {
 		{"this is a test to test the fuzzy Filter", "tf", true},  // normal selection
 		{"this is a test to test the fuzzy Filter", "wp", false}, // incorrect selection
 		{"THIS IS A TEST TO TEST THE FUZZY FILTER", "tu", true},  // case insensitivity
+		{"this is a Test to test the fuzzy filter", "Tu", true},  // case sensitivity
+		{"this is a Test to test the fUzzy filter", "TU", true},  // case sensitivity
+		{"this is a test to test the fuzzy filter", "Tu", false}, // case sensitivity
+		{"this is a test to Test the fuzzy filter", "TU", false}, // case sensitivity
 		{"日本語は難しいです", "難", true},                                 // kanji
 		{"あ、日本語は難しいですよ", "あい", true},                             // hiragana
 		{"パソコンは遅いですネ", "ソネ", true},                               // katana
