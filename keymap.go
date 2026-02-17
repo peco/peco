6037ac2ee50f173b94435c890c075c0b7e20f152
Daisuke Maki <lestrrat+github@gmail.com>
2026-02-17 14:44:49 +0900

    Avoid action-at-a-distance when we don't need to

--- keymap.go
+++ keymap.go
@@ -127,7 +127,6 @@ func (km Keymap) resolveActionName(name string, depth int) (Action, error) {
 			actions = append(actions, child)
 		}
 		v = makeCombinedAction(actions...)
-		nameToActions[name] = v
 		return v, nil
 	}
 
