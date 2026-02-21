14530444b1ce790460ac15a454bb81adf18b1bd8
Daisuke Maki <lestrrat+github@gmail.com>
2026-02-21 16:04:30 +0900

    appease linter

--- options.go
+++ options.go
@@ -14,25 +14,25 @@ import (
 
 // CLIOptions holds the command-line flags parsed by go-flags.
 type CLIOptions struct {
-	OptHelp            bool   `short:"h" long:"help" description:"show this help message and exit"`
-	OptQuery           string `long:"query" description:"initial value for query"`
-	OptRcfile          string `long:"rcfile" description:"path to the settings file"`
-	OptVersion         bool   `long:"version" description:"print the version and exit"`
-	OptBufferSize      int    `long:"buffer-size" short:"b" description:"number of lines to keep in search buffer"`
-	OptEnableNullSep   bool   `long:"null" description:"expect NUL (\\0) as separator for target/output"`
-	OptInitialIndex    int    `long:"initial-index" description:"position of the initial index of the selection (0 base)"`
-	OptInitialFilter   string `long:"initial-filter" description:"specify the default filter"`
-	OptPrompt          string `long:"prompt" description:"specify the prompt string"`
-	OptLayout          string `long:"layout" description:"layout to be used. 'top-down', 'bottom-up', or 'top-down-query-bottom'. default is 'top-down'"`
-	OptSelect1         bool   `long:"select-1" description:"select first item and immediately exit if the input contains only 1 item"`
-	OptExitZero        bool   `long:"exit-0" description:"exit immediately with status 1 if the input is empty"`
-	OptSelectAll       bool   `long:"select-all" description:"select all items and immediately exit"`
-	OptOnCancel        string `long:"on-cancel" description:"specify action on user cancel. 'success' or 'error'.\ndefault is 'success'. This may change in future versions"`
-	OptSelectionPrefix string `long:"selection-prefix" description:"use a prefix instead of changing line color to indicate currently selected lines.\ndefault is to use colors. This option is experimental"`
-	OptExec            string `long:"exec" description:"execute command instead of finishing/terminating peco.\nPlease note that this command will receive selected line(s) from stdin,\nand will be executed via '/bin/sh -c' or 'cmd /c'"`
-	OptPrintQuery      bool   `long:"print-query" description:"print out the current query as first line of output"`
+	OptHelp            bool             `short:"h" long:"help" description:"show this help message and exit"`
+	OptQuery           string           `long:"query" description:"initial value for query"`
+	OptRcfile          string           `long:"rcfile" description:"path to the settings file"`
+	OptVersion         bool             `long:"version" description:"print the version and exit"`
+	OptBufferSize      int              `long:"buffer-size" short:"b" description:"number of lines to keep in search buffer"`
+	OptEnableNullSep   bool             `long:"null" description:"expect NUL (\\0) as separator for target/output"`
+	OptInitialIndex    int              `long:"initial-index" description:"position of the initial index of the selection (0 base)"`
+	OptInitialFilter   string           `long:"initial-filter" description:"specify the default filter"`
+	OptPrompt          string           `long:"prompt" description:"specify the prompt string"`
+	OptLayout          string           `long:"layout" description:"layout to be used. 'top-down', 'bottom-up', or 'top-down-query-bottom'. default is 'top-down'"`
+	OptSelect1         bool             `long:"select-1" description:"select first item and immediately exit if the input contains only 1 item"`
+	OptExitZero        bool             `long:"exit-0" description:"exit immediately with status 1 if the input is empty"`
+	OptSelectAll       bool             `long:"select-all" description:"select all items and immediately exit"`
+	OptOnCancel        string           `long:"on-cancel" description:"specify action on user cancel. 'success' or 'error'.\ndefault is 'success'. This may change in future versions"`
+	OptSelectionPrefix string           `long:"selection-prefix" description:"use a prefix instead of changing line color to indicate currently selected lines.\ndefault is to use colors. This option is experimental"`
+	OptExec            string           `long:"exec" description:"execute command instead of finishing/terminating peco.\nPlease note that this command will receive selected line(s) from stdin,\nand will be executed via '/bin/sh -c' or 'cmd /c'"`
+	OptPrintQuery      bool             `long:"print-query" description:"print out the current query as first line of output"`
 	OptColor           config.ColorMode `long:"color" description:"color mode: 'auto' (default, parse ANSI codes) or 'none' (disable)" default:"auto"`
-	OptHeight          string `long:"height" description:"display height in lines or percentage (e.g. '10', '50%')"`
+	OptHeight          string           `long:"height" description:"display height in lines or percentage (e.g. '10', '50%')"`
 }
 
 // parse parses command-line arguments and validates the resulting options.
